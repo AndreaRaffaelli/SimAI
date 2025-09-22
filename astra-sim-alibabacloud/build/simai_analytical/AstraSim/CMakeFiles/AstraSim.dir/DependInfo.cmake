09fd71f403c673832e5b78da4b72dc41adb37458
cyber://a <billal.mokhtari@etu.uca.fr>
2025-09-22 17:35:21 +0200

    fix stability to common.h

---
+++ astra-sim-alibabacloud/build/simai_analytical/AstraSim/CMakeFiles/AstraSim.dir/DependInfo.cmake
@@ -0,0 +1,80 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/AstraParamParse.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/AstraParamParse.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/AstraParamParse.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/BaseStream.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/BaseStream.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/BaseStream.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/BasicEventHandlerData.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/BasicEventHandlerData.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/BasicEventHandlerData.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/BootStrapnet.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/BootStrapnet.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/BootStrapnet.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/CollectivePhase.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/CollectivePhase.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/CollectivePhase.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/DMA_Request.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/DMA_Request.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/DMA_Request.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/DataSet.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/DataSet.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/DataSet.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/IntData.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/IntData.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/IntData.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/LogGP.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/LogGP.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/LogGP.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/MemBus.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/MemBus.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/MemBus.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/MemMovRequest.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/MemMovRequest.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/MemMovRequest.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/MockNcclChannel.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/MockNcclChannel.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/MockNcclChannel.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/MockNcclGroup.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/MockNcclGroup.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/MockNcclGroup.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/MockNcclLog.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/MockNcclLog.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/MockNcclLog.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/MyPacket.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/MyPacket.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/MyPacket.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/NetworkStat.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/NetworkStat.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/NetworkStat.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/PacketBundle.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/PacketBundle.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/PacketBundle.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/PhyMultiThread.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/PhyMultiThread.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/PhyMultiThread.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/QueueLevelHandler.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/QueueLevelHandler.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/QueueLevelHandler.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/QueueLevels.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/QueueLevels.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/QueueLevels.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/RecvPacketEventHadndlerData.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/RecvPacketEventHadndlerData.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/RecvPacketEventHadndlerData.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/RendezvousRecvData.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/RendezvousRecvData.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/RendezvousRecvData.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/RendezvousSendData.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/RendezvousSendData.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/RendezvousSendData.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/SendPacketEventHandlerData.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/SendPacketEventHandlerData.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/SendPacketEventHandlerData.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/SharedBusStat.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/SharedBusStat.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/SharedBusStat.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/SimAiFlowModelRdma.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/SimAiFlowModelRdma.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/SimAiFlowModelRdma.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/SimRecvCaller.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/SimRecvCaller.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/SimRecvCaller.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/SimSendCaller.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/SimSendCaller.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/SimSendCaller.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/StatData.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/StatData.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/StatData.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/StreamBaseline.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/StreamBaseline.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/StreamBaseline.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/StreamStat.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/StreamStat.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/StreamStat.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/Sys.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/Sys.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/Sys.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/Usage.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/Usage.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/Usage.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/UsageTracker.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/UsageTracker.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/UsageTracker.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/calbusbw.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/calbusbw.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/calbusbw.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/collective/Algorithm.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/collective/Algorithm.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/collective/Algorithm.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/collective/AllToAll.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/collective/AllToAll.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/collective/AllToAll.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/collective/DoubleBinaryTreeAllReduce.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/collective/DoubleBinaryTreeAllReduce.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/collective/DoubleBinaryTreeAllReduce.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/collective/HalvingDoubling.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/collective/HalvingDoubling.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/collective/HalvingDoubling.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/collective/NcclTreeFlowModel.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/collective/NcclTreeFlowModel.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/collective/NcclTreeFlowModel.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/collective/Ring.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/collective/Ring.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/collective/Ring.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/fast-backend/FastBackEnd.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/fast-backend/FastBackEnd.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/fast-backend/FastBackEnd.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/memory/SimpleMemory.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/memory/SimpleMemory.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/memory/SimpleMemory.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/scheduling/OfflineGreedy.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/scheduling/OfflineGreedy.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/scheduling/OfflineGreedy.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/topology/BasicLogicalTopology.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/BasicLogicalTopology.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/BasicLogicalTopology.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/topology/BinaryTree.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/BinaryTree.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/BinaryTree.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/topology/ComplexLogicalTopology.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/ComplexLogicalTopology.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/ComplexLogicalTopology.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/topology/DoubleBinaryTreeTopology.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/DoubleBinaryTreeTopology.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/DoubleBinaryTreeTopology.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/topology/GeneralComplexTopology.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/GeneralComplexTopology.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/GeneralComplexTopology.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/topology/LocalRingGlobalBinaryTree.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/LocalRingGlobalBinaryTree.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/LocalRingGlobalBinaryTree.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/topology/LocalRingNodeA2AGlobalDBT.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/LocalRingNodeA2AGlobalDBT.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/LocalRingNodeA2AGlobalDBT.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/topology/LogicalTopology.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/LogicalTopology.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/LogicalTopology.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/topology/Node.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/Node.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/Node.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/topology/RingTopology.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/RingTopology.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/RingTopology.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/system/topology/Torus3D.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/Torus3D.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/system/topology/Torus3D.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/workload/CSVWriter.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/workload/CSVWriter.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/workload/CSVWriter.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/workload/Layer.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/workload/Layer.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/workload/Layer.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/workload/Workload.cc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/workload/Workload.cc.o" "gcc" "AstraSim/CMakeFiles/AstraSim.dir/astra-sim/workload/Workload.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ astra-sim-alibabacloud/build/simai_analytical/simai_analytical/CMakeFiles/SimAI_analytical.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/network_frontend/analytical/AnaSim.cc" "simai_analytical/CMakeFiles/SimAI_analytical.dir/AnaSim.cc.o" "gcc" "simai_analytical/CMakeFiles/SimAI_analytical.dir/AnaSim.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/network_frontend/analytical/AnalyticalAstra.cc" "simai_analytical/CMakeFiles/SimAI_analytical.dir/AnalyticalAstra.cc.o" "gcc" "simai_analytical/CMakeFiles/SimAI_analytical.dir/AnalyticalAstra.cc.o.d"
+  "/home/roc/SimAI/astra-sim-alibabacloud/astra-sim/network_frontend/analytical/AnalyticalNetwork.cc" "simai_analytical/CMakeFiles/SimAI_analytical.dir/AnalyticalNetwork.cc.o" "gcc" "simai_analytical/CMakeFiles/SimAI_analytical.dir/AnalyticalNetwork.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
